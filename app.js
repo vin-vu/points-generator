1245d52193839fa017cd5947be43a83dd0a9c62a
vin-vu <vince.hvu@gmail.com>
2024-11-24 15:20:55 -0800

    Added dev dependencies

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
@@ -11,22 +11,6 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Receipt processor listening on port ${port}!`);
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
