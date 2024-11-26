9626fb628007fdf603c5a5a6e18c77a7140dc7a0
vin-vu <vince.hvu@gmail.com>
2024-11-26 11:52:35 -0800

    Adding 50 points if receipt total is round number

--- src/helper/pointsHelper.js
+++ src/helper/pointsHelper.js
@@ -1,9 +1,20 @@
 module.exports.calculateTotalPoints = (receipt) => {
+  let pointsTotal = 0;
 
   const retailer = receipt.retailer;
-  const alphanumericCountRetailer = retailer.split('').filter(char => /[a-zA-Z0-9]/.test(char)).length;
-  console.log('alphanumeric count: ', alphanumericCountRetailer)
+  const alphanumericCountRetailer = retailer
+    .split("")
+    .filter((char) => /[a-zA-Z0-9]/.test(char)).length;
+  pointsTotal += alphanumericCountRetailer;
 
+  const receiptTotal = Number(receipt.total);
 
-  console.log('receipt: ', JSON.stringify(receipt))
-}
+  if (receiptTotal % 1 === 0) {
+    pointsTotal += 50;
+    console.log("no cents");
+  }
+
+  console.log("pointsTotal: ", pointsTotal);
+
+  console.log("receipt: ", JSON.stringify(receipt));
+};
