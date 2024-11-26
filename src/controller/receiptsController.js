eaee029c27d89bf0886a0f9e8a1a26be05db838b
vin-vu <vince.hvu@gmail.com>
2024-11-25 20:44:37 -0800

    Storing receipts in memory

--- src/controller/receiptsController.js
+++ src/controller/receiptsController.js
@@ -1,4 +1,8 @@
+const idStorage = require("../data/receiptStorage");
+
 exports.receiptIdCreateGet = (req, res, next) => {
-  const { id } = req;
+  const { body, id } = req;
+  idStorage[id] = body;
+  console.log("id storage: ", JSON.stringify(idStorage));
   res.send({ id });
 };
---
+++ src/data/receiptStorage.js
@@ -0,0 +1,3 @@
+const receiptStorage = {}
+
+module.exports = receiptStorage;
