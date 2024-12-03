031c621a52406850bff8bce154169318206a9c1a
vin-vu <vince.hvu@gmail.com>
2024-12-03 01:26:16 -0800

    Testing for handling 404 errors at /points route

--- src/middleware/globalErrorHandler.test.js
+++ src/middleware/globalErrorHandler.test.js
@@ -6,16 +6,23 @@ const app = express();
 app.use(express.json());
 
 // Simulate an error directly assuming valid receipt
-app.post("/process", (req, res, next) => {
+app.post("/receipts/process", (req, res, next) => {
   const error = new Error("Unexpected error");
-  next(error)
+  return next(error);
+});
+
+app.get("/receipts/:id/points", (req, res, next) => {
+  const error = new Error("No receipt found for that id");
+  error.status = 404;
+  error.details = "No receipt found for that id";
+  return next(error);
 });
 
 // Attach the global error handler
 app.use(globalErrorHandler);
 
-describe('Global Error Handler', () => {
-  it('Should return 500 for unexpected errors with valid receipt', async () => {
+describe("Global Error Handler", () => {
+  it("Should handle unexpected errors from /process route", async () => {
     const validReceipt = {
       retailer: "Target",
       purchaseDate: "2022-01-01",
@@ -24,8 +31,14 @@ describe('Global Error Handler', () => {
       total: "35.35",
     };
 
-    const res = await request(app).post('/process').send(validReceipt);
-    expect(res.status).toBe(500)
-    expect(res.body).toEqual({error: "Internal Server Error"})
-  })
-})
+    const res = await request(app).post("/receipts/process").send(validReceipt);
+    expect(res.status).toBe(500);
+    expect(res.body).toEqual({ error: "Internal Server Error" });
+  });
+
+  it("Should handle 404 errors from /:id/points route", async () => {
+    const res = await request(app).get("/receipts/invalid123/points");
+    expect(res.status).toBe(404);
+    expect(res.body).toEqual({ error: "No receipt found for that id" });
+  });
+});
