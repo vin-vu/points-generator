caf18c36eb3208c2936335e3163ed4f11e17ec2f
vin-vu <vince.hvu@gmail.com>
2024-12-03 12:40:19 -0800

    Updates to controller unit tests

--- src/controller/receiptsController.test.js
+++ src/controller/receiptsController.test.js
@@ -1,85 +1,142 @@
-jest.mock("../data/receiptStorage", () => ({}));
-
 const receiptStorage = require("../data/receiptStorage");
-
 const { saveReceipt, getPoints } = require("../controller/receiptsController");
+const { calculateTotalPoints } = require("../helper/pointsHelper");
+const { validationResult } = require("express-validator");
+
+jest.mock("../data/receiptStorage", () => ({}));
+jest.mock("express-validator", () => ({
+  validationResult: jest.fn(),
+}));
+jest.mock("../helper/pointsHelper", () => ({
+  calculateTotalPoints: jest.fn().mockReturnValue(42),
+}));
 
 describe("Receipts Controller", () => {
   describe("saveReciept", () => {
-    it("should save receipt and return the ID", () => {
-      const mockReq = { body: { retailer: "Target" }, id: "123" };
+    beforeEach(() => {
+      jest.clearAllMocks();
+      for (const key in receiptStorage) delete receiptStorage[key];
+    });
+
+    it("should save a valid receipt and return the ID", () => {
+      // Mock request and response
+      const mockReq = {
+        body: {
+          retailer: "Target",
+          purchaseDate: "2022-01-01",
+          purchaseTime: "13:01",
+          items: [{ shortDescription: "Mountain Dew 12PK", price: "6.49" }],
+          total: "35.35",
+        },
+        id: "123",
+      };
       const mockRes = { send: jest.fn() };
       const mockNext = jest.fn();
+
+      // Mock validationResult to return no errors
+      validationResult.mockReturnValue({
+        isEmpty: () => true,
+      });
 
       saveReceipt(mockReq, mockRes, mockNext);
 
       // Assertions
-      expect(receiptStorage).toHaveProperty("123", { retailer: "Target" }); // Receipt was saved
+      expect(receiptStorage).toHaveProperty("123", mockReq.body); // Receipt saved correctly
       expect(mockRes.send).toHaveBeenCalledWith({ id: "123" }); // Correct response sent
-      expect(mockNext).not.toHaveBeenCalled(); // No error occured
+      expect(mockNext).not.toHaveBeenCalled(); // No error occurred
     });
-  });
 
-  describe("getPoints", () => {
-    it("should return points for a valid receipt ID", () => {
-      // Populate mock receiptStorage
-      receiptStorage["123"] = {
-        retailer: "Target",
-        purchaseDate: "2022-01-01",
-        purchaseTime: "13:01",
-        items: [
+    it("should return error for invalid receipt", () => {
+      // Mock request and response
+      const mockReq = {
+        body: {
+          retailer: "Target!", // Invalid retailer
+          purchaseDate: "2022-01-01",
+          purchaseTime: "13:01",
+          items: [{ shortDescription: "Mountain Dew 12PK", price: "6.49" }],
+          total: "35.35",
+        },
+        id: "123",
+      };
+      const mockRes = { send: jest.fn() };
+      const mockNext = jest.fn();
+
+      // Mock validationResult to return error
+      validationResult.mockReturnValue({
+        isEmpty: () => false,
+        errors: [
           {
-            shortDescription: "Mountain Dew 12PK",
-            price: "6.49",
-          },
-          {
-            shortDescription: "Emils Cheese Pizza",
-            price: "12.25",
-          },
-          {
-            shortDescription: "Knorr Creamy Chicken",
-            price: "1.26",
-          },
-          {
-            shortDescription: "Doritos Nacho Cheese",
-            price: "3.35",
-          },
-          {
-            shortDescription: "   Klarbrunn 12-PK 12 FL OZ  ",
-            price: "12.00",
+            type: "field",
+            value: "Target!",
+            msg: "Retailer must be a valid alphanumeric string",
+            path: "retailer",
+            location: "body",
           },
         ],
-        total: "35.35",
-      };
+      });
 
-      const mockReq = { params: { id: "123" } };
-      const mockRes = { send: jest.fn() };
-      const mockNext = jest.fn();
-
-      getPoints(mockReq, mockRes, mockNext);
+      saveReceipt(mockReq, mockRes, mockNext);
 
       // Assertions
-      expect(mockRes.send).toHaveBeenCalledWith(
-        expect.objectContaining({ points: expect.any(Number) })
-      ); // Points Returned
-      expect(mockNext).not.toHaveBeenCalled(); // No error occured
+      expect(receiptStorage).not.toHaveProperty("123"); // Receipt not saved
+      expect(mockRes.send).not.toHaveBeenCalled(); // No response sent
+      expect(mockNext).toHaveBeenCalled(); // Error handling invoked
+      const error = mockNext.mock.calls[0][0];
+      expect(error).toHaveProperty("status", 400);
+      expect(error).toHaveProperty("details", [
+        {
+          type: "field",
+          value: "Target!",
+          msg: "Retailer must be a valid alphanumeric string",
+          path: "retailer",
+          location: "body",
+        },
+      ]);
     });
 
-    it("should return 404 with error message for invald receipt ID", () => {
-      const mockReq = { params: { id: "abc" } };
-      const mockRes = { send: jest.fn() };
-      const mockNext = jest.fn();
+    describe("getPoints", () => {
+      it("should return points for a valid receipt ID", () => {
+        // Populate mock receiptStorage
+        receiptStorage["123"] = {
+          retailer: "Target",
+          purchaseDate: "2022-01-01",
+          purchaseTime: "13:01",
+          items: [{ shortDescription: "Mountain Dew 12PK", price: "6.49" }],
+          total: "35.35",
+        };
 
-      getPoints(mockReq, mockReq, mockNext);
+        const mockReq = { params: { id: "123" } };
+        const mockRes = { send: jest.fn() };
+        const mockNext = jest.fn();
 
-      // Assertions
-      expect(mockNext).toHaveBeenCalledWith(
-        expect.objectContaining({
-          status: 404,
-          details: "No receipt found for that id",
-        })
-      ); // Pass 404 error to next global error handler
-      expect(mockRes.send).not.toHaveBeenCalledWith(); // No response sent
+        getPoints(mockReq, mockRes, mockNext);
+
+        // Assertions
+        expect(calculateTotalPoints).toHaveBeenCalledWith(
+          receiptStorage["123"]
+        );
+        expect(mockRes.send).toHaveBeenCalledWith(
+          expect.objectContaining({ points: expect.any(Number) })
+        ); // Points Returned
+        expect(mockNext).not.toHaveBeenCalled(); // No error occured
+      });
+
+      it("should return 404 with error message for invald receipt ID", () => {
+        const mockReq = { params: { id: "abc" } };
+        const mockRes = { send: jest.fn() };
+        const mockNext = jest.fn();
+
+        getPoints(mockReq, mockReq, mockNext);
+
+        // Assertions
+        expect(mockNext).toHaveBeenCalledWith(
+          expect.objectContaining({
+            status: 404,
+            details: "No receipt found for that id",
+          })
+        ); // Pass 404 error to next global error handler
+        expect(mockRes.send).not.toHaveBeenCalledWith(); // No response sent
+      });
     });
   });
 });
