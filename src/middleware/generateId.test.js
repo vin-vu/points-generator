8b9897dd33ffcda1bde73481760c123924c69df5
vin-vu <vince.hvu@gmail.com>
2024-12-03 15:25:57 -0800

    Cleanup generatedId test

--- src/middleware/generateId.test.js
+++ src/middleware/generateId.test.js
@@ -2,19 +2,15 @@ const { v4: uuidv4, validate } = require("uuid");
 const generateUniqueId = require("./generateId");
 const express = require("express");
 const request = require("supertest");
+const app = require("../app");
 
 describe("genenrateUniqueId Middleware", () => {
-  let app;
-
-  beforeEach(() => {
-    app = express();
+  it("should generate unique ID and attach to req", async () => {
     app.use(generateUniqueId); // use middleware
     app.get("/", (req, res) => {
       res.status(200).send({ id: req.id }); // send ID to the response
     });
-  });
 
-  it("should generate unique ID and attach to req", async () => {
     const res = await request(app).get("/");
 
     // ensure response contains valid UUID using uuid.validate
